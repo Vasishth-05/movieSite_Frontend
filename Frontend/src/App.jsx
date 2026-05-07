ce0fe844b4d014ae84b712640b22a407fb614c34
Vasishth <learnvasishth@gmail.com>
2026-05-07 10:26:24 +0530

    add favourites file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import Favourites from './Pages/Favourites'
 import Home from './Pages/Home'
+import {Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <Home/>
-    </>
+    <main className='mainContent'>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/favourites' element={<Favourites/>} />
+      </Routes>
+    </main>
   )
 }
 
---
+++ Frontend/src/Pages/Favourites.jsx
@@ -0,0 +1,8 @@
+function Favourites(){
+    return <div className="favourites">
+        <h3>No favourte movies here yet</h3>
+        <p>Start adding movies here...</p>
+    </div>
+}
+
+export default Favourites;
